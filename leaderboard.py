35a18960cdd009aaee8827b829c4adb06ec9dd34
Brandon Rozek <brozek@brandonrozek.com>
2024-01-06 13:58:11 -0500

    Fixed none issue

--- leaderboard.py
+++ leaderboard.py
@@ -14,12 +14,15 @@ if __name__ == "__main__":
     args = vars(parser.parse_args())
 
     # If not specified, then use today's date
-    DATE = args.get("date", str(datetime.today().date()))
+    DATE = args.get("date")
+    if DATE is None:
+        DATE = str(datetime.today().date())
 
     con = sqlite3.connect(WordGuess.RESULTS_LOCATION)
     try:
         cur = con.cursor()
         res = cur.execute(f"SELECT user, score FROM scores WHERE date = '{DATE}' ORDER BY score DESC")
+        print(f"High scores for date '{DATE}'")
         for username, score in res.fetchall():
               print(username, score)
     finally:
